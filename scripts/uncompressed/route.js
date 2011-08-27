ad47895f47851b07ef6763f3c9a4ef0e541f5548
Matt McLaughlin <mclaughlin.matt@gmail.com>
2011-08-27 14:09:16 -0700

    formatting updates

--- scripts/uncompressed/route.js
+++ scripts/uncompressed/route.js
@@ -1,85 +1,109 @@
 /**
-* Route.js Core
-* @author Matthew Jon McLaughlin <mclaughlin.matt@gmail.com>
-* @copyright 2011 Matthew Jon McLaughlin <mclaughlin.matt@gmail.com>
-*
-**/
+ * Route.js Core
+ * 
+ * @author Matthew Jon McLaughlin <mclaughlin.matt@gmail.com>
+ * @copyright 2011 Matthew Jon McLaughlin <mclaughlin.matt@gmail.com>
+ * 
+ */
 
-(function(window, undefined){
-    
-    var
-        History = window.History||undefined,
-        Route = window.Route = window.Route||{};
-    
-    // Check Existence
-    if ( typeof Route.init !== 'undefined'  ) {
-        throw new Error("Route.js Core has already been loaded.");
-    }
+(function(window, undefined) {
 
-    
-    // Initialize Route
-    Route.init = function(){
-        // Check History Requirement
-        if( typeof History === 'undefined' || !History.enabled ){
-            throw new Error("Route.js requires History.js Core");
-        }
-        
-        /**
-        * Route.routes
-        * Url to resource object map
-        **/
-        Route.routes = Route.routes||{};
+	var History = window.History || undefined, Route = window.Route = window.Route
+			|| {};
 
-        /**
-        * Route.rootUrl
-        * Root url for the application
-        **/
-        Route.rootUrl = History.getRootUrl();
+	// Check Existence
+	if (typeof Route.init !== 'undefined') {
+		throw new Error("Route.js Core has already been loaded.");
+	}
 
-       /**
-       * Route.run()
-       * Starts the router listening to statechanges in the location bar
-       * and routes the existing url
-       **/
-       Route.run = function( routes ){
-            // Set Routes
-            Route.routes = routes||{};
-            
-            // Route initial url
-            Route.dispatch()
-            
-            // Listen for statechanges
-            History.Adapter.bind(window,'statechange',function() {
-                Route.dispatch();
-            });
-       };
-       
-       Route.dispatch = function(){
-            var
-                State = History.getState(),
-                url = State.url,
-                relativeUrl = url.replace(Route.rootUrl, ''),
-                relativeUrl = relativeUrl ? '/'+relativeUrl : '/';
-            Route.routes[relativeUrl]();
-       }
+	// Initialize Route
+	Route.init = function() {
+		// Check History Requirement
+		if (typeof History === 'undefined' || !History.enabled) {
+			throw new Error("Route.js requires History.js Core");
+		}
 
-       /**
-       * Attach History pushState to onClick events
-       **/
-       Route.attachStateChangeEvents = function(){
-            links = document.getElementsByTagName('a');
-            for(var i=0;i<links.length;i++){
-                links[i].onclick = function(){
-                    var 
-                        title = this.title,
-                        url = this.href;
+		/**
+		 * Route.routes 
+		 * 
+		 * Url to resource object map
+		 */
+		Route.routes = Route.routes || {};
 
-                    History.pushState(undefined, title, url);
-                    return false;
-                }
-            }
-       };
-    }
-    
-    Route.init();
+		/**
+		 * Route.rootUrl 
+		 * 
+		 * Root url for the application
+		 */
+		Route.rootUrl = History.getRootUrl();
+
+		/**
+		 * Route.run(routes) 
+		 * 
+		 * Starts the router listening to statechanges in the
+		 * location bar and routes the existing url
+		 * 
+		 * @param {Object} routes - Map of urls and their resources
+		 * @return
+		 */
+		Route.run = function(routes) {
+			// Set Routes
+			Route.addRoutes(routes || {});
+
+			// Route initial url
+			Route.dispatch();
+
+			// Listen for statechanges
+			History.Adapter.bind(window, 'statechange', function() {
+				Route.dispatch();
+			});
+		};
+
+		/**
+		 * Route.addRoute(routes) 
+		 * 
+		 * Adds routes to the route table. Will overwrite
+		 * existing routes.
+		 * 
+		 * @param {Object} routes - Map of urls and their resources
+		 * @return
+		 */
+		Route.addRoutes = function(routes) {
+			for (prop in routes) {
+				Route.routes[prop] = routes[prop];
+			}
+		};
+
+		/**
+		 * Route.dispatch() 
+		 * 
+		 * Evaluates the resource mapped to the current url
+		 */
+		Route.dispatch = function() {
+			var 
+				State = History.getState(), 
+				url = State.url, 
+				relativeUrl = url.replace(Route.rootUrl, ''), 
+				matchUrl = relativeUrl ? '/'+relativeUrl	: '/';
+			
+			Route.routes[matchUrl]();
+		};
+
+		/**
+		 * Attach History pushState to onClick events
+		 */
+		Route.attachStateChangeEvents = function() {
+			links = document.getElementsByTagName('a');
+			for ( var i = 0; i < links.length; i++) {
+				links[i].onclick = function() {
+					var title = this.title, url = this.href;
+
+					History.pushState(undefined, title, url);
+					return false;
+				};
+			}
+		};
+	};
+
+	Route.init();
 })(window);
